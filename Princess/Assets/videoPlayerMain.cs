442041f36f4faba41bc6f9f756e89235c7c79154
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-01-23 12:48:55 -0600

    Set up link to github, finished main menu animation, git ignore art

---
+++ Princess/Assets/videoPlayerMain.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class videoPlayerMain : MonoBehaviour
+
+{
+    public VideoPlayer videoPlayer;
+    public float loopStartTime = 2f;
+    public float loopEndTime = 5f;
+    private bool isFirstLoop = true;
+
+    void Start(){
+        
+        GameObject videoPlayerGO = GameObject.Find("Video Player");
+        videoPlayer = videoPlayerGO.GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += VideoEnded;
+        
+    }
+
+    void VideoEnded(VideoPlayer vp)
+    {
+        if (isFirstLoop)
+        {
+            videoPlayer.time = loopStartTime;
+            videoPlayer.playbackSpeed = 1f;
+            
+            
+        }
+        videoPlayer.time = loopStartTime;
+        videoPlayer.isLooping = true;
+        videoPlayer.Play();
+    }
+}
