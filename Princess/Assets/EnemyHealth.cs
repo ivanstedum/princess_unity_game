054f31550b8f32ddca376fdcb232e14dc90c693e
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-01-31 14:58:58 -0600

    enemy health however it isnt wokring, taking damage animations stuck

---
+++ Princess/Assets/EnemyHealth.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int damage;
+    private Animator enemyAnimator;
+    private bool isDamaged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        
+        if(other.collider.CompareTag("Player"))
+        {
+            Animator playerAnimator =  other.collider.GetComponent<Animator>();
+            AnimatorStateInfo stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
+        
+            if (stateInfo.IsName("princess-slash") && !isDamaged)
+                {
+                    Debug.Log("work");
+                    health -= damage;
+                    EnemyDamage();
+                    if (health <= 0)
+                    {
+                        enemyAnimator.SetBool("ghostDeath", true);
+                    }
+                }
+            else {
+                Debug.Log("not work");
+                enemyAnimator.SetBool("ghostDamage", false);
+            }
+        
+        }
+    }
+    private void Update()
+    {
+        if (isDamaged && !enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("ghostDamage"))
+        {
+            enemyAnimator.SetBool("ghostMovesTowardsPlayer", true);
+            isDamaged = false;
+        }
+    }
+    private void EnemyDamage()
+    {
+        Debug.Log("inside");
+        enemyAnimator.SetBool("ghostMovesTowardsPlayer", false);
+        enemyAnimator.SetBool("ghostDamage", true);
+        isDamaged = true;
+    }
+    
+    private void EnemyDeath()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+}
