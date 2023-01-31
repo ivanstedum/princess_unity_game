95a09aa18ba0732eb3d6c79cdffa649f76f88d87
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-01-30 18:09:22 -0600

    enemy patrol, needs to be refactored but for now uses boxCollider to keep enemy within range and changes animation when player in raange

--- Princess/Assets/EnemyPatrol.cs
+++ Princess/Assets/EnemyPatrol.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class EnemyPatrol : MonoBehaviour
 {
     [SerializeField] private GameObject patrolArea;
-    [SerializeField]private float speed = 1f;
+    [SerializeField] private float speed = 1f;
     private bool facingRight = false;
+    private Transform playerTransform;
 
+  void Start()
+{
+    playerTransform = GameObject.FindWithTag("Player").transform;
+    if (playerTransform == null)
+    {
+        Debug.LogError("No GameObject with tag 'Player' found.");
+    }
+}
+    
 private void Update()
 {
     float minX = patrolArea.GetComponent<BoxCollider2D>().bounds.min.x;
     float maxX = patrolArea.GetComponent<BoxCollider2D>().bounds.max.x;
-
+    float minY = patrolArea.GetComponent<BoxCollider2D>().bounds.min.y;
+    float maxY = patrolArea.GetComponent<BoxCollider2D>().bounds.max.y;
 
     if (transform.position.x <= minX || transform.position.x >= maxX)
     {
@@ -21,6 +32,37 @@ private void Update()
     }
 
     float targetX = facingRight ? maxX : minX;
-    transform.position = Vector2.MoveTowards(transform.position, new Vector2(targetX, transform.position.y), speed * Time.deltaTime);
+
+    // Check if the player is within the patrol area
+    if (playerTransform.position.x >= minX && playerTransform.position.x <= maxX &&
+        playerTransform.position.y >= minY && playerTransform.position.y <= maxY)
+    {
+        // Follow the player
+        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+
+        // Face the player
+        if (playerTransform.position.x > transform.position.x && !facingRight)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            facingRight = true;
+        }
+        else if (playerTransform.position.x < transform.position.x && facingRight)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            facingRight = false;
+        }
+
+        // Trigger the running animation
+        GetComponent<Animator>().SetBool("ghostMovesTowardsPlayer", true);
+    }
+    else
+    {
+        // Move towards the patrol point
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(targetX, transform.position.y), speed * Time.deltaTime);
+
+        // Stop the running animation
+        GetComponent<Animator>().SetBool("ghostMovesTowardsPlayer", false);
+    }
 }
+
 }
