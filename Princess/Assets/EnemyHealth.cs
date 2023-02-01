806cc7c7bca601491d341d7b9df56c5759203554
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-02-01 10:07:20 -0600

    changed onColloisionEnter to onCollisionStay

--- Princess/Assets/EnemyHealth.cs
+++ Princess/Assets/EnemyHealth.cs
@@ -15,7 +15,7 @@ public class EnemyHealth : MonoBehaviour
         enemyAnimator = GetComponent<Animator>();
     }
     
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionStay2D(Collision2D other) {
         
         if(other.collider.CompareTag("Player"))
         {
