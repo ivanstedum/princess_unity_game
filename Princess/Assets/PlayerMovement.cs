de5c376903b7266ed8237c2703832d0dfc97213e
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-02-02 16:09:51 -0600

    removed debug statement

--- Princess/Assets/PlayerMovement.cs
+++ Princess/Assets/PlayerMovement.cs
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         
-        Debug.Log("Hello, world");
+        
         playerRb =  GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
