b171b83fb66be7333caae292085a757606ddd13a
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2023-02-03 21:26:14 -0600

    attached fireball to enemy, with changes to direct

--- Princess/Assets/EnemyFire.cs
+++ Princess/Assets/EnemyFire.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EnemyFire : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private EnemyPatrol EnemyPatrol;
+    [SerializeField] private GameObject enemy;
     void Start()
     {
+    
+        EnemyPatrol = enemy.GetComponent<EnemyPatrol>();
+        if (EnemyPatrol == null)
+    {
+        Debug.LogError("EnemyPatrol component not found!");
+        return;
+    }
         
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
+{
+    EnemyPatrol = enemy.GetComponent<EnemyPatrol>();
+    Debug.Log($"Enemy Patrol : {EnemyPatrol.EnemyFacingRight}");
+    DirectionOfFireBall();
+}
+private void DirectionOfFireBall()
+{
+    if (EnemyPatrol.EnemyFacingRight)
     {
-        
+        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
     }
+    else if (!EnemyPatrol.EnemyFacingRight)
+    {
+        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+}
+
+
+
 }
